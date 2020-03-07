f210ed4965e118b2e02f417ce567382b4215a06e
Jeremy Prater <silverevo03@gmail.com>
2020-03-07 00:26:12 -0800

    Closer...

--- client/darksky-client/src/components/NavMenu.vue
+++ client/darksky-client/src/components/NavMenu.vue
@@ -1,41 +1,48 @@
 <template>
   <div>
-    <Navbar bg="dark" expand="lg" class="navbar-dark StatusBar">
-      <FontAwesomeIcon icon="{faSatelliteDish}" size="2x" color="orange" class="LogoIcon" />
-      <Navbar class="Brand LogoText" href="#home">
-        <h2>DarkSky</h2>
-      </Navbar>
-      <Navbar class="Toggle" aria-controls="basic-navbar-nav" />
-      <Navbar class="Collapse" id="basic-navbar-nav">
-        <Nav class="mr-auto">
-          <Nav class="Link" href="#manual">Manual Tracking</Nav>
-          <Nav class="Link" href="#target">Align to target</Nav>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">
+        <FontAwesomeIcon icon="{faSatelliteDish}" size="2x" color="orange" class="LogoIcon" />DarkSky
+      </b-navbar-brand>
 
-          <ComportMenu state="{this.state}" />
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-          <Button variant="outline-danger">
-            <FontAwesomeIcon icon="{faPlug}" size="lg" color="orange" />
-          </Button>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Manual Tracking</b-nav-item>
+          <b-nav-item href="#">Align To Target</b-nav-item>
+        </b-navbar-nav>
 
-          <NavDropdown title="LNB Power" id="lnb_power">
-            <NavDropdown class="Item" href="#lnb/off">Off</NavDropdown>
-            <NavDropdown class="Item" href="#lnb/auto">Auto</NavDropdown>
-            <NavDropdown class="Divider" />
-            <NavDropdown class="Item" href="#lnb/0">Mode 0 : +13 vdc @ 0 Hz</NavDropdown>
-            <NavDropdown class="Item" href="#lnb/1">Mode 1 : +13 vdc @ 22 KHz</NavDropdown>
-            <NavDropdown class="Item" href="#lnb/2">Mode 2 : +18 vdc @ 22 KHz</NavDropdown>
-            <NavDropdown class="Item" href="#lnb/3">Mode 3 : +18 vdc @ 0 KHz</NavDropdown>
-          </NavDropdown>
-          <Button variant="outline-danger">
-            <FontAwesomeIcon icon="{faPowerOff}" size="lg" color="gray" />
-          </Button>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" class="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-      </Navbar>
-    </Navbar>
+        <b-nav-item-dropdown text="Comm Port">
+          <b-dropdown-item href="#">/dev/tty1</b-dropdown-item>
+          <b-dropdown-item href="#">/something/else/v2</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-button variant="outline-danger">
+          <FontAwesomeIcon icon="{faPlug}" size="lg" color="orange" />
+        </b-button>
+
+        <b-nav-item-dropdown text="LNB Power">
+          <b-dropdown-item href="#">Off</b-dropdown-item>
+          <b-dropdown-item href="#">Auto</b-dropdown-item>
+          <b-dropdown-item href="#">Mode 0 : +13 vdc @ 0 Hz</b-dropdown-item>
+          <b-dropdown-item href="#">Mode 1 : +13 vdc @ 22 KHz</b-dropdown-item>
+          <b-dropdown-item href="#">Mode 2 : +18 vdc @ 22 KHz</b-dropdown-item>
+          <b-dropdown-item href="#">Mode 3 : +18 vdc @ 0 KHz</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-button variant="outline-danger">
+          <FontAwesomeIcon icon="{faPowerOff}" size="lg" color="gray" />
+        </b-button>
+      </b-collapse>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+      </b-navbar-nav>
+    </b-navbar>
   </div>
 </template>
 
@@ -47,16 +54,14 @@ import { mapState } from "vuex";
 export default {
   name: "NavMenu",
   data() {
-    return {
-    };
+    return {};
   },
   //components: {  },
   computed: mapState({
     project: state => state.project,
     currentWorkspace: state => state.currentWorkspace
   }),
-  methods: {
-  }
+  methods: {}
 };
 </script>
 
