c2b6c92260ff0d489ce8dd0cde76b996de05523f
Jeremy Prater <silverevo03@gmail.com>
2020-06-12 16:44:18 -0700

    - Update build_env.sh

--- server/build_env.sh
+++ server/build_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf server_runtime
-python -m venv server_runtime
+python3 -m venv server_runtime
 source server_runtime/bin/activate
 
 pip3 install coloredlogs pyserial python-socketio eventlet
@@ -9,6 +9,7 @@ cp *.py server_runtime
 pip3 freeze > requirements.txt
 
 if [ $1 == "generate" ]; then
-    rm -rf /work/yoctoland/meta-darksky/recipes-darksky/darksky-server-python/*
+    rm -rf /work/yoctoland/meta-darksky/recipes-darksky/darksky-server-python/
+    mkdir -p /work/yoctoland/meta-darksky/recipes-darksky/darksky-server-python/
     pipoe --python python3 -r requirements.txt  -o /work/yoctoland/meta-darksky/recipes-darksky/darksky-server-python
 fi
