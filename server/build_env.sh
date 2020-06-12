f3056c530048336e69035462fdd1a7e9e9231d1e
Jeremy Prater <silverevo03@gmail.com>
2020-06-12 13:42:33 -0700

    - Little bit of work

--- server/build_env.sh
+++ server/build_env.sh
@@ -4,3 +4,7 @@ rm -rf server_runtime
 python -m venv server_runtime
 source server_runtime/bin/activate
 pip3 install coloredlogs pyserial python-socketio eventlet
+pip3 freeze > requirements.txt
+
+rm -rf /work/yoctoland/meta-darksky/recipes-darksky/darksky-server-python/*
+pipoe --python python3 -r requirements.txt  -o /work/yoctoland/meta-darksky/recipes-darksky/darksky-server-python
