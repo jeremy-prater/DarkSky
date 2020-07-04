6d2317a2c02f1cdb27825c937277e4f952d84b2b
Jeremy Prater <silverevo03@gmail.com>
2020-07-04 13:10:23 -0700

    - More state work

--- socket-server/state.py
+++ socket-server/state.py
@@ -2,10 +2,18 @@ import logging
 from backend_socketio import SocketIOBackend
 import time
 import threading
+from packet import Packet, PacketCommand
 
 
 class State:
     instance = None
+
+    MOTOR_STATES = [
+        'stopped',
+        'forward',
+        'reverse',
+        'stall'
+    ]
 
     @staticmethod
     def getInstance():
@@ -25,14 +33,15 @@ class State:
                 'carrier': False
             },
             'motors': {
+                'stopAll': False,
                 'ra': {
+                    'state': 'unknown',
                     'position': 0,
-                    'state': 'stopped',
                     'stopAt': 0
                 },
                 'dec': {
+                    'state': 'unknown',
                     'position': 0,
-                    'state': 'stopped',
                     'stopAt': 0
                 }
             },
@@ -49,14 +58,15 @@ class State:
                 'carrier': False
             },
             'motors': {
+                'stopAll': False,
                 'ra': {
-                    'position': 0,
                     'state': 'stopped',
+                    'position': 0,
                     'stopAt': 0
                 },
                 'dec': {
-                    'position': 0,
                     'state': 'stopped',
+                    'position': 0,
                     'stopAt': 0
                 }
             }
@@ -76,9 +86,100 @@ class State:
         while (context.sending):
             context.logger.info('Emitting state over socket')
             context.logger.info(context.state)
+
             time.sleep(1)
 
     def updateGPS(self, gpsFix):
         # self.logger.info(gpsFix)
         # self.logger.info('GPS Update {}.{} @ {}'.format(gpsFix['lat'], gpsFix['lon'], gpsFix['time']))
         self.state['gps'] = gpsFix
+
+        # Here we need to signal the astronomical coordinates to update
+        # 1. time changed
+        # 2. coordinates may have changed
+        # 3. altiude correction
+        # 4. precession
+        #
+        # ... do math
+
+    # Control board state updates
+
+    # Tick rate 500ms
+
+    # order of commands
+    # packet.command = MOTOR_DEC_STATE;
+    # packet.arg1 = darkSkyContext.motor1.state;
+
+    # packet.command = MOTOR_DEC_POSITION;
+    # packet.arg1 = darkSkyContext.motor1.position;
+
+    # packet.command = MOTOR_DEC_STOP_POS;
+    # packet.arg1 = darkSkyContext.motor1.stopPosition;
+
+    # packet.command = MOTOR_RA_STATE;
+    # packet.arg1 = darkSkyContext.motor2.state;
+
+    # packet.command = MOTOR_RA_POSITION;
+    # packet.arg1 = darkSkyContext.motor2.position;
+
+    # packet.command = MOTOR_RA_STOP_POS;
+    # packet.arg1 = darkSkyContext.motor2.stopPosition;
+
+    # packet.command = LNB_STATE;
+    # packet.arg1 = darkSkyContext.lnb.power;
+    # packet.arg2 = darkSkyContext.lnb.carrier;
+
+    # State update methods
+
+    def updateDecState(self, packet: Packet):
+        self.updateMotorState('dec', packet)
+
+    def updateRaState(self, packet: Packet):
+        self.updateMotorState('ra', packet)
+
+    def requestDecState(self, packet: Packet):
+        self.requestMotorState('dec', packet)
+
+    def requestRaState(self, packet: Packet):
+        self.requestMotorState('ra', packet)
+
+    def updateMotorState(self, motor: str, packet: Packet)
+    {
+        payload = packet.GetPayload()
+        newState = payload['arg1']
+
+        self.state.state['motors'][motor]['state'] = State.MOTOR_STATES[newState]
+    }
+
+    def requestMotorState(self, motor: str, state: str)
+    {
+        self.state.requestedState['motors'][motor]['state'] = state
+        self.processStateUpdate()
+    }
+
+    def processStateUpdate(self):
+        # LNB State
+        if self.requestedState['lnb']['voltage'] != self.state['lnb']['voltage']:
+            pass
+        if self.requestedState['lnb']['carrier'] != self.state['lnb']['carrier']:
+            pass
+
+        # Stop all motor state
+        if self.requestedState['motor']['stopAll'] != self.state['motor']['stopAll']:
+            pass
+
+        # RA motor state
+        if self.requestedState['motor']['ra']['state'] != self.state['motor']['ra']['state']:
+            pass
+        if self.requestedState['motor']['ra']['position'] != self.state['motor']['ra']['position']:
+            pass
+        if self.requestedState['motor']['ra']['state'] != self.state['motor']['ra']['state']:
+            pass
+
+        # Dec motor state
+        if self.requestedState['motor']['dec']['state'] != self.state['motor']['dec']['state']:
+            pass
+        if self.requestedState['motor']['dec']['position'] != self.state['motor']['dec']['position']:
+            pass
+        if self.requestedState['motor']['dec']['state'] != self.state['motor']['dec']['state']:
+            pass
